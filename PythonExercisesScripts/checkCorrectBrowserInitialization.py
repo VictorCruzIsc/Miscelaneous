457eb18beeb431b045c870a1bcc636983252f169
Nelson Victor Cruz Hernandez <victor.cruz.isc@gmail.com>
2016-11-02 16:54:17 -0600

    Add subprocess checking
    The code has an error, it was assuming that all xdg-open processes have
    a subprocess but this was not true all the time.
    
    Subprocess check existance was added to avoid runtime exceptions.
    
    Search of the subprocess id is performed cat /proc/<process_id>/cmd-line
    to avoid using another application like grep.
    
    Signed-off-by: Nelson Victor Cruz Hernandez <victor.cruz.isc@gmail.com>

--- PythonExercisesScripts/checkCorrectBrowserInitialization.py
+++ PythonExercisesScripts/checkCorrectBrowserInitialization.py
@@ -1,59 +1,56 @@
 import subprocess
 import shlex
+import time
+import os
 
-PSTREE =   "pstree -p"
+PROC_SEARCH = "/proc/{0}/cmdline"
+PSTREE_SEARCH = "pstree -p | grep "
 XDG_OPEN = "xdg-open"
-GREP =     "grep "
-PS =       "ps -ax"
+URL_CHECK = ("http://google.com")
+DELIMITER = "\x00"
 
-def get_subprocess_id(process):
-    first_closing_parenthesis = process.find(")")
-    second_opening_parenthesis = process.find("(", first_closing_parenthesis)
-    second_closing_parenthesis = process.find(")",
-        first_closing_parenthesis + 1)
-    start = (second_opening_parenthesis + 1)
-    end = -(len(process) - second_closing_parenthesis)
+def get_xdg_subprocess_id(process):
+    #process e.g: `-xdg-open(63141)---<browser>(<pid>)+-{<browser>}(63251)
+    xdg_pos = process.find(XDG_OPEN)
+    xdg_closing_parenthesis = process.find(")", xdg_pos)
+    subprocess_opening_parenthesis = process.find("(", xdg_closing_parenthesis)
+    if subprocess_opening_parenthesis == -1:
+        return None
+    subprocess_closing_parenthesis = process.find(")",
+        subprocess_opening_parenthesis)
+    start = (subprocess_opening_parenthesis + 1)
+    end = -(len(process) - subprocess_closing_parenthesis)
     return process[start:end]
 
-def execute_pipe_commands(commands):
-    total_commands = len(commands)
-    if total_commands > 0:
-        args = shlex.split(commands[0])
-        execution = subprocess.Popen(args, stdout = subprocess.PIPE)
-        commands_range = range(1, total_commands)
-        for x in commands_range:
-            args = shlex.split(commands[x])
-            consecutive_execution = subprocess.Popen(args,
-                stdin = execution.stdout, stdout = subprocess.PIPE)
-            execution = consecutive_execution
-        return [line.rstrip() for line in execution.stdout]
-    else:
-        return []
+def execute_command(command):
+    execution = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    return [line.strip() for line in execution.stdout]
 
 def validate_process_parameter(process, parameter):
-    subprocess_id = get_subprocess_id(process)
-    grep_child_process_id = execute_pipe_commands([PS,
-        "{0}{1}".format(GREP, subprocess_id)])
-    return any(parameter in result for result in grep_child_process_id)
+    subprocess_id = get_xdg_subprocess_id(process)
+    if subprocess_id is None:
+        return False
+    subprocess_cmdline_path = PROC_SEARCH.format(subprocess_id)
+    if not os.path.exists(subprocess_cmdline_path):
+        return False
+    calling_cmd = open(subprocess_cmdline_path).read().split(DELIMITER)
+    return calling_cmd[1] == parameter
 
 def validate_web_process_execution(url):
-    pstree_results = execute_pipe_commands([PSTREE,
-        "{0}{1}".format(GREP, XDG_OPEN)])
+    pstree_results = execute_command("{0}{1}".format(PSTREE_SEARCH, XDG_OPEN))
     if any(XDG_OPEN in result for result in pstree_results):
-        xdg_processes = [xdg_process
-            for xdg_process in pstree_results if XDG_OPEN in xdg_process]
         valid_parameters = [validate_process_parameter(process, url)
-            for process in xdg_processes]
+            for process in pstree_results]
         return valid_parameters.count(True) >= 1
     else:
+        test.log("Any xdg-open process was found")
         return False
 
 def main():
-    url = "http://stackoverflow.com"
-    if validate_web_process_execution(url):
-        print "OK"
+    if validate_web_process_execution(URL_CHECK):
+        print "Ok"
     else:
         print "ERROR"
 
 if __name__ == "__main__":
-   main()
+    main()
