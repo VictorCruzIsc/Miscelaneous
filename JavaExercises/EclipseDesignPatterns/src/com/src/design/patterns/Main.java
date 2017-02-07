64df4c143e657e17d731bbfaed38c4bde7ff8783
Nelson Victor Cruz Hdez <victor.cruz.isc@gmail.com>
2017-02-07 12:37:43 -0600

    Testing .gitignore

---
+++ JavaExercises/EclipseDesignPatterns/src/com/src/design/patterns/Main.java
@@ -0,0 +1,10 @@
+package com.src.design.patterns;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Comething to post");
+    }
+
+}
